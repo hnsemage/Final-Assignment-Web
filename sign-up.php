8e6f01984ff7f154611291820012a4a6d7aaf5ab
thuthimarian <74374348+thuthimarian@users.noreply.github.com>
2020-12-29 01:53:09 +0530

    Create sign-up.php

---
+++ sign-up.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'dbh.php'; 
+if(isset($_REQUEST["submit"])){
+
+$name=$_REQUEST["uname"];
+$email=$_REQUEST["email"];
+$pword=$_REQUEST["pword"];
+$cpwd=$_REQUEST["cpass"];
+
+}
+
+
+ 
+
+
+
+
+?>
